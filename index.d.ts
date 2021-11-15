923ef1fd4298ce6bb38618c27ca0565654001c36
Nicholai Nissen <nicholainissen@gmail.com>
2021-11-15 17:19:23 +0100

    fix: use unknown return values

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module '@ocalan/graceful' {
   interface GracefulOptions {
-    servers?: Array<{ close(): any }>;
-    mongooses?: Array<{ disconnect(): any }>;
-    brees?: Array<{ stop(): any }>;
-    customHandlers?: Array<() => any>;
+    servers?: Array<{ close(): unknown }>;
+    mongooses?: Array<{ disconnect(): unknown }>;
+    brees?: Array<{ stop(): unknown }>;
+    customHandlers?: Array<() => unknown>;
   }
   export default class Graceful {
     constructor(options: GracefulOptions);
